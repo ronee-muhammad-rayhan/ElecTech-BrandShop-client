78ecb54ab2fd2da58a57a8085136bea038e0fd22
RoneeMRayhan <roneemrayhan@rayhanworld.com>
2023-10-18 18:56:30 +0600

    demo banner and footer added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,10 @@
+import Banner from "../../shared/Banner";
 
 const Home = () => {
     return (
         <div>
             <h3 className="text-3xl">Home</h3>
+            <Banner></Banner>
         </div>
     );
 };
