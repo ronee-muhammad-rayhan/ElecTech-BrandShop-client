6239300395c4be5e6bc680fd24e55eef0f2d7ad8
RoneeMRayhan <roneemrayhan@rayhanworld.com>
2023-10-18 17:16:26 +0600

    router separeted

--- src/routes/public/AppRoutes.jsx
+++ src/routes/public/AppRoutes.jsx
@@ -1,10 +1,37 @@
+import Root from "../../outlines/Root";
+import ErrorPage from "../../pages/ErrorPage/ErrorPage";
+import Home from "../../pages/Home/Home";
+import Login from "../../pages/Login/Login";
+import NotFound from "../../pages/NotFoundPage/NotFound";
+import Register from "../../pages/Register/Register";
+import {
+    createBrowserRouter,
+} from "react-router-dom";
 
-const AppRoutes = () => {
-    return (
-        <div>
+const router = createBrowserRouter([
+    {
+        path: "/",
+        errorElement: <ErrorPage></ErrorPage>,
+        element: <Root />,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>
+            },
+            {
+                path: '/register',
+                element: <Register></Register>
+            },
+            {
+                path: '/login',
+                element: <Login></Login>
+            },
+            {
+                path: '*',
+                element: <NotFound></NotFound>
+            },
+        ],
+    },
+]);
 
-        </div>
-    );
-};
-
-export default AppRoutes;
+export default router;
