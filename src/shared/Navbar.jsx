2268ba563deb8bb676e50060c7e84efdba3c72b8
RoneeMRayhan <roneemrayhan@rayhanworld.com>
2023-10-20 16:48:36 +0600

    Profile Avatar shown

--- src/shared/Navbar.jsx
+++ src/shared/Navbar.jsx
@@ -73,7 +73,7 @@ const Navbar = () => {
                     <Link to={`/profile`}>
                         <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
                             <div className="w-10 rounded-full">
-                                <img src={userDefaultPic} />
+                                <img src={user.photoURL || userDefaultPic} />
                             </div>
                         </label>
                     </Link>
